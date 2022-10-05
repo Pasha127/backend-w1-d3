7005b958682c5e5fc26c11a7bdee3d26b5aac1b9
Pasha127 <pasha.account@gmail.com>
2022-10-05 23:43:47 +0200

    basic and first extra finished

--- src/authors/index.js
+++ src/authors/index.js
@@ -4,49 +4,69 @@ import { dirname, join } from "path";
 import { fileURLToPath } from "url";
 import uniqid from "uniqid";
 
-const authorsJSONPath = join(dirname(fileURLToPath(import.meta.url)),authors.json)
+const authorsJSONPath = join(dirname(fileURLToPath(import.meta.url)), "authors.json")
 
 const authorRouter = express.Router();
 
-authorRouter.get("/",(req,res)=>{
-    const allContent = fs.readFileSync(authorsJSONPath);
-    const authors = JSON.parse(allContent);
-    response.status(200).send(authors)
+authorRouter.get("/",(req,res,next)=>{
+    try{
+        const authors = JSON.parse(fs.readFileSync(authorsJSONPath));
+        res.status(200).send(authors)
+    }catch(error){
+        res.status(500).send(error)
+    }
+
 })
 
 authorRouter.get("/:authorId" , (req,res)=>{
+    try{
     const authorId = req.params.authorId;
     const authorsArray = JSON.parse(fs.readFileSync(authorsJSONPath));
     const foundAuthor = authorsArray.find(author => author.id === authorId);
-    response.status(200).send(foundAuthor);
+    res.status(200).send(foundAuthor);
+}catch(error){
+    res.status(500).send(error)
+}
 })
 
 authorRouter.post("/", (req,res)=>{
+    try{
     const newAuthor = {...req.body, createdAt:new Date(), id:uniqid()};
     const authorsArray = JSON.parse(fs.readFileSync(authorsJSONPath))
     authorsArray.push(newAuthor);
     fs.writeFileSync(authorsJSONPath, JSON.stringify(authorsArray));
-    response.status(201).send(`Added a new author with an "id" of: ${newAuthor.id}`);
+    res.status(201).send(`Added a new author with an "id" of: ${newAuthor.id}`);
+}catch(error){
+    res.status(500).send(error)
+}
 })
 
 authorRouter.put("/:authorId", (req,res)=>{
+    try{
     const authorsArray = JSON.parse(fs.readFileSync(authorsJSONPath));
     const entryIndex = authorsArray.findIndex(author => author.id === req.params.authorId);
     const oldAuthor = authorsArray[entryIndex];
     if(oldAuthor.email=== req.body.email){
-        response.status(208).send("An author with this email already exists.")
+        res.status(208).send("An author with this email already exists.")
     }else{
         const updatedAuthor = {...oldAuthor, ...req.body, updatedAt:new Date()}
         authorsArray[entryIndex] = updatedAuthor;
         fs.writeFileSync(authorsJSONPath, JSON.stringify(authorsArray));
-        response.status(201).send(`The author has ben updated:`, updatedAuthor)
+        res.status(201).send(`The author has ben updated:`, updatedAuthor)
     }
+}catch(error){
+    res.status(500).send(error)
+}
 })
 
-authorRouter.delete("/:authorId", (req,res)=>{
+authorRouter.delete("/:authorId", (req,res)=>{try{
     const authorsArray = JSON.parse(fs.readFileSync(authorsJSONPath));
     const remainingAuthors = authorsArray.filter(author => author.id !== req.params.authorId);
     fs.writeFileSync(authorsJSONPath,JSON.stringify(remainingAuthors));
-    response.status(204).send("Author has been deleted.")
+    res.status(204).send("Author has been deleted.")
+}catch(error){
+    res.status(500).send(error)
+}
 })
 
+export default authorRouter;
--- src/server.js
+++ src/server.js
@@ -1,15 +1,20 @@
 import express from "express";
+import cors from "cors";
 import listEndpoints from "express-list-endpoints";
-import authorRouter from "./api/index.js";
-
+import authorRouter from "./authors/index.js";
 const server = express();
+server.use(cors());
+server.use(express.json())
 const port = 3001
 
 server.use(express.json())
-
 server.use("/authors", authorRouter)
 
 server.listen( port, ()=>{
     console.table(listEndpoints(server))
     console.log("server is running on port:" , port)
 })
+
+server.on("error", (error)=>
+console.log(`Server not running due to ${error}`)
+)
